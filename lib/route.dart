387c64042295c46ebb4d732a0e5388f2dd22d5f1
sanjyot242 <sanjyot.satvi242@gmail.com>
2024-02-25 02:52:49 -0800

    Fixed merge Conflict

--- lib/route.dart
+++ lib/route.dart
@@ -3,11 +3,13 @@ import 'package:rescuemate/screens/main_screen.dart';
 import 'package:rescuemate/screens/splash_screen.dart';
 import 'package:rescuemate/screens/sos_info_screen.dart';
 import 'package:rescuemate/screens/edit_sos_info_screen.dart';
+import 'package:rescuemate/screens/card_widget.dart';
 
 const String splashScreen = 'splash_screen';
 const String mainScreen = 'main_screen';
 const String sosInfoScreen = 'sos_info_screen';
 const String editSosInfoScreen = 'edit_sos_info_screen';
+const String cardScreen = 'card_widget';
 
 Route<dynamic> controller(RouteSettings settings) {
   final args = settings.arguments;
@@ -20,6 +22,8 @@ Route<dynamic> controller(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => SosInfoScreen());
     case editSosInfoScreen:
       return MaterialPageRoute(builder: (context) => EditSosInfoScreen());
+    case cardScreen:
+      return MaterialPageRoute(builder: (context) => CardExampleApp());
     default:
       throw ('No such route exists');
   }
