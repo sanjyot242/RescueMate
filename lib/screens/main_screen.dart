1a3bf85ae2609f2dcce4e653251eca210fae3e5a
Abhinav Singh <Abhinav@MacBook-Air-126.local>
2024-02-25 07:27:56 -0800

    gave up here will continue later

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -105,18 +105,6 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
-  Future<List<dynamic>> fetchData() async {
-    final response = await http.get(Uri.parse('http://localhost:3000/api/nearby'));
-    print(response.body);
-    if (response.statusCode == 200) {
-      // If the server returns a 200 OK response, parse the JSON as a list
-      return json.decode(response.body);
-    } else {
-      // If the server did not return a 200 OK response, throw an exception
-      throw Exception('Failed to load data');
-    }
-  }
-
   void _exitApp() {
     SystemNavigator.pop();
   }
