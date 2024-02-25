472e8881ba98b4b916d582b4615d8880d3c2e0cb
sanjyot242 <sanjyot.satvi242@gmail.com>
2024-02-25 03:54:38 -0800

    Updated backedn API

--- backend/server.js
+++ backend/server.js
@@ -35,54 +35,18 @@ function interpretOpenStatus(closedBucket) {
     }
 }
 
-app.get('/api/hospitals/nearby', async (req, res) => {
-    try {
-       // const { latitude, longitude } = req.query;
-       const latitude = 33.87029; 
-       const longitude = -117.92534;
-        if (!latitude || !longitude) {
-            return res.status(400).json({ error: 'Latitude and longitude are required' });
-        }
-
-        const url = `https://api.foursquare.com/v2/venues/search?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${VERSION}&ll=${latitude},${longitude}&query=hospital&intent=browse&radius=10000`; // radius in meters
-
-        const foursquareResponse = await axios.get(url);
-        const hospitals = foursquareResponse.data.response.venues.map(venue => ({
-            name: venue.name,
-            location: venue.location
-        }));
-
-        res.json({ hospitals });
-    } catch (error) {
-        console.error('Error:', error.message);
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.error('API response error:', error.response.data);
-            res.status(error.response.status).json({ error: error.response.data });
-        } else if (error.request) {
-            // The request was made but no response was received
-            console.error('API no response error:', error.request);
-            res.status(500).json({ error: 'No response from server' });
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.error('API setup error:', error.message);
-            res.status(500).json({ error: 'Error setting up API request' });
-        }
-    }
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-async function placeSearch(latitude, longitude) {
+async function placeSearch(latitude, longitude , queryValue ) {
     try {
         const searchParams = new URLSearchParams({
-            query: 'hospital',
+            query: `${queryValue}`,
             ll: `${latitude},${longitude}`,
             sort: 'DISTANCE',
-            radius:'100000'
+            radius:'100000',
         });
 
         const results = await fetch(
@@ -105,16 +69,17 @@ async function placeSearch(latitude, longitude) {
     }
 }
 
-app.get('/api/hospitals/nearby2', async (req, res) => {
+app.get('/api/nearby', async (req, res) => {
     try {
-       // const { latitude, longitude } = req.query;
+       // const { latitude, longitude , type } = req.query;
         const latitude = 33.87029; 
         const longitude = -117.92534;
+        const type = "pharmacy"
         if (!latitude || !longitude) {
             return res.status(400).json({ error: 'Latitude and longitude are required' });
         }
 
-        const data = await placeSearch(latitude, longitude);
+        const data = await placeSearch(latitude, longitude, type);
         
         res.json(data);
     } catch (error) {
